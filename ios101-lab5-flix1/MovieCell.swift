7d3400f6fcd0ed11154bb2f9a40806b24e543c5f
Eric Connelly <therealericle@gmail.com>
2024-03-19 22:02:05 -0400

    finish lab 5

---
+++ ios101-lab5-flix1/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  ios101-lab5-flix1
+//
+//  Created by Eric Connelly on 3/19/24.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
